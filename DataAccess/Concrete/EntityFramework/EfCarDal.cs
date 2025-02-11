5ac4d9eb2bb8e8ab892619443f574d874638bc40
Aykut Şahin <sahinaykut97@gmail.com>
2025-02-11 15:18:52 +0300

    Added Payment Service

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,41 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails(Expression<Func<CarDetailDto, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from car in context.Cars
+                             join b in context.Brands
+                             on car.BrandId equals b.BrandId
+                             join cl in context.Colors
+                             on car.ColorId equals cl.ColorId
+                             select new CarDetailDto
+                             {
+                                 CarId = car.CarId,
+                                 BrandId = b.BrandId,
+                                 BrandName = b.BrandName,
+                                 ColorId = cl.ColorId,
+                                 ColorName = cl.ColorName,
+                                 DailyPrice = car.DailyPrice,
+                                 ModelYear = car.ModelYear,
+                                 Description = car.Descriptions,
+                                 ImagePath = (from im in context.CarImages where im.CarId == car.CarId select im.ImagePath).FirstOrDefault()
+                             };
+                return filter == null ? result.ToList() : result.Where(filter).ToList();
+            }
+        }
+    }
+}
